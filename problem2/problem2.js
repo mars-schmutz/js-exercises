b18f134bb00190eba96aff0c9cf873d35a4cab46
Marshall Schmutz <darthmars97@gmail.com>
2024-06-20 11:19:16 -0600

    update problem2

--- problem2/problem2.js
+++ problem2/problem2.js
@@ -8,5 +8,7 @@ const programs = [
 
 const list = document.getElementById("programs");
 for (let i = 0; i < porgrams.length; i++) {
-    let li = document.getElementById("li");
+    let li = document.createElement("li");
+    li.innerText = programs[0];
+    ul.appendChild(li);
 }
