0646e383f226fe158e6f4c2e1c42ef484654941e
Marshall Schmutz <darthmars97@gmail.com>
2024-06-20 09:02:37 -0600

    reorganize problems

---
+++ problem1/problem1.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById("change-btn");
+const p = document.getElementById("txt");
+
+button.addEventListener("click", () => {
+    p.innerText = "I've been changed!";
+});
---
+++ problem2/problem2.js
@@ -0,0 +1,12 @@
+const programs = [
+    "Web Development",
+    "IT",
+    "Digital Media Design",
+    "App Development",
+    "Drafting and Design",
+];
+
+const list = document.getElementById("programs");
+for (let i = 0; i < porgrams.length; i++) {
+    let li = document.getElementById("li");
+}
